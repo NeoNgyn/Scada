9d872129a29f1ae015edf80f5f77abdbd140b4f5
NhanNPSE184696 <nguyenphucnhan2004@gmail.com>
2026-06-28 23:17:52 +0700

    feat: connect DB

---
+++ FINAL_G17/FINAL_G17/DAL/Database.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace FINAL_G17.DAL
+{
+    public class Database
+    {
+        public static SqlConnection GetConnection()
+        {
+            string connStr =
+                ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+
+            return new SqlConnection(connStr);
+        }
+    }
+}
