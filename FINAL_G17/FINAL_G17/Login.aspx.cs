47478019bf082eec011ec6b102795e5e78df3708
NhanNPSE184696 <nguyenphucnhan2004@gmail.com>
2026-06-29 10:30:26 +0700

    feat: login logic

--- FINAL_G17/FINAL_G17/Login.aspx.cs
+++ FINAL_G17/FINAL_G17/Login.aspx.cs
@@ -1,17 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Data.SqlClient;
 using System.Web.UI;
-using System.Web.UI.WebControls;
+using FINAL_G17.DAL; 
 
 namespace FINAL_G17
 {
-    public partial class WebForm1 : System.Web.UI.Page
+    public partial class WebForm1 : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+        }
 
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                ShowMessage("Vui lòng nhập tài khoản và mật khẩu.");
+                return;
+            }
+
+            if (CheckLogin(username, password))
+            {
+                Session["Username"] = username;
+                Response.Redirect("~/Default.aspx");
+            }
+            else
+            {
+                ShowMessage("Tài khoản hoặc mật khẩu không đúng.");
+            }
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Register.aspx");
+        }
+
+        private bool CheckLogin(string username, string password)
+        {
+            string query = "SELECT COUNT(1) FROM Account WHERE Username = @Username AND [Password] = @Password";
+
+            using (SqlConnection conn = Database.GetConnection()) 
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@Username", username);
+                cmd.Parameters.AddWithValue("@Password", password);
+
+                conn.Open();
+                int count = (int)cmd.ExecuteScalar();
+                return count > 0;
+            }
+        }
+
+        private void ShowMessage(string message)
+        {
+            lblMessage.Text = message;
+            lblMessage.Visible = true;
         }
     }
 }
